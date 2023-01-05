5b5bb2a89ef5f17202a5a8017ee09fe8085caea1
SophieNordell <113043401+SophieNordell@users.noreply.github.com>
2023-01-05 14:33:56 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Clock from "./Clock";
 
 function App() {
-  return <div></div>;
-}
+  //använd useEffekt med tom array när du vill köra kod en gång
+  //när komponenten skapas.
+  // useEffekt används för att hämta api-anrop.
+  // används för att starta setInterval.
 
+  return (
+    <div>
+      <Clock city="London" timeZone="Europe/London" />
+      <Clock city="Stockholm" timeZone="Europe/Stockholm" />
+    </div>
+  );
+}
 export default App;
