4d85a2e083972d6989172b9d3f8f06b9e8ada997
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-07 02:23:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import Clock from "./Clock.jsx";
 function App() {
-  return <div></div>;
+  return (
+        <div className="app"> 
+            <Clock      timeZoneProp = "Europe/Stockholm" />
+            <Clock     timeZoneProp = "America/New_York" />
+            <Clock      timeZoneProp= "Europe/Paris" />
+            <Clock       timeZoneProp = "Europe/London" />
+        </div>
+  );
 }
 
 export default App;
