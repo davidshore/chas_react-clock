a4885d2e0d2bc39c9a7ebe4896f18f342b00fbd4
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-01-07 21:19:15 +0100

    Update Clock.jsx

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-export default function Clock( {timeZoneProp } ) {
+export default function Clock(props) {
     const [nowTime, setNowTime] = useState()
     useEffect(() => {
         setInterval(() => {
                 const date = new Date()
-                setNowTime(date.toLocaleTimeString('eu-SE',{timeZone : timeZoneProp }))
+                setNowTime(date.toLocaleTimeString('eu-SE',{timeZone : props.timeZoneProp }))
         }, 1000);
     }, []);
 
@@ -19,7 +19,7 @@ export default function Clock( {timeZoneProp } ) {
         setTime(false)
     }
     return (<div className='clock'>
-                <h2> {timeZoneProp }</h2>
+                <h2> {props.city} </h2>
                 <button type='button' id='btn' onClick={open}>See Time</button>
                 <button type='button' id='btn' onClick={close}>Close Time</button>
                 { showTime && <div className='timeShow'><h1>{nowTime}</h1></div> }
