bd7615e6cf49e6f32167293162f968e631ae1383
Jack Lindhammar <jacklindhammar94@gmail.com>
2023-01-11 20:01:12 +0100

    Jack commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
+import Clock from "./clock";
+
 
 function App() {
-  return <div></div>;
-}
+
+
+  return (
+    <div>
+      <Clock city = "Piteå" timeZoneProp = "Europe/Piteå" />
+      
+    </div>
+  )
+};
 
 export default App;
